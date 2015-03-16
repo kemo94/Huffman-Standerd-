e4abe391c62b34e85844c26e74a9a73a2b5bef93
kemo94 <kareem.hussein94@yahoo.com>
2015-03-16 11:35:51 +0200

    Update MainWindow.xaml.cs

--- huffman/MainWindow.xaml.cs
+++ huffman/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Collections;
-namespace huffman
+
+namespace WpfApplication1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -26,181 +27,119 @@ namespace huffman
             InitializeComponent();
         }
 
-        public Dictionary<string, int> MapDic = new Dictionary<string, int>(); // dictionary for Compression
-        public Dictionary<string, string> CompMap = new Dictionary<string, string>(); // Compression result
+        Dictionary<char, string> MapDic; // dictioanry
+        List<char> Allchar;// all char without duplicate
+        BinaryTree BT; 
 
-        public List<string> AllChar = new List<string>(); // All Charcters without duplicate 
+        string text = "";
 
-        string text = ""; // take data from user
-        public int back = 0;
-        public int NumOfChar = 0;
-
-        void getDuplicated() // get sum of duplicated char 
+        // read dictionary
+        void readDic()
         {
+            MapDic = new Dictionary<char, string>();
+        
+            string []Dic = dic.Text.Split();
 
-            Dictionary<string, int> Map = new Dictionary<string, int>();
-            Map.Add((screen.Text[0] + ""), 1);
-            var items = from pair in Map orderby pair.Value descending select pair;
-            int check = 0;
-            for (int i = 1; i < screen.Text.Length; i++)
-            {
+            for (int i = 0; i < Dic.Length; i += 2)
+                MapDic.Add(char.Parse(Dic[i]), Dic[i+1] );
 
-
-                items = from pair in Map orderby pair.Value descending select pair;
-
-                foreach (KeyValuePair<string, int> pair in items) // count repeated char
-                {
-                    if (pair.Key != screen.Text[i] + "") // if not found add it in map
-
-                        check = 1;
-
-                    else // if found increase the counter
-                    {
-                        Map[pair.Key]++;
-
-                        check = 0;
-                        break;
-                    }
-                }
-
-
-
-                if (check == 1)
-                {
-
-                    Map.Add(screen.Text[i] + "", 1);
-                    check = 0;
-                }
-            }
-
-
-            //sort map
-            items = from pair in Map orderby pair.Value descending select pair;
-            foreach (KeyValuePair<string, int> pair in items)
-            {
-                MapDic.Add(pair.Key, pair.Value);
-                AllChar.Add(pair.Key);
-                 
-            }
-            NumOfChar = Map.Count;
-        }
-
-        public Dictionary<string, int> UpdateDic()
-        {
-
-
-            List<string> listChar = new List<string>(MapDic.Keys); 
-            Dictionary<string, int> BxMap = new Dictionary<string, int>(); // update map
-
-            int  sum = 0;
-            string chars = "";
-            for (int indx = 0; indx < listChar.Count; indx ++)
-            {
-                if (indx == MapDic.Count - 2 || indx == MapDic.Count - 1)
-                {
-                    chars = listChar[indx] + chars;
-                    sum += MapDic[listChar[indx]];
-                }
-
-                else
-
-                    BxMap.Add(listChar[indx], MapDic[listChar[indx]]);
-
-
-
-                if (indx == MapDic.Count - 1) // add last two string together 
-
-                    BxMap.Add(chars, sum);
-
-            }
-            // sort map
-            var items = from pair in BxMap orderby pair.Value descending select pair;
-            MapDic.Clear();
-            AllChar.Clear();
-
-            foreach (KeyValuePair<string, int> pair in items)
-            {
-                MapDic.Add(pair.Key, pair.Value);
-                AllChar.Add(pair.Key);
-
-            }
-            return MapDic;
+            
         }
 
         // Compression
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
 
-            text = screen.Text;
-            getDuplicated(); // count sum of duplicated char
-
-
-            Dictionary<string, int> AlterMap = new Dictionary<string, int>(MapDic);
-            List<string>  AlterAllChar = new List<string>(AllChar);
-
-            while (MapDic.Count != 2 ) // decrease the map to reach 0 , 1 (left & right) (binary tree) 
+            if (dic.Text.Length == 0)
+                MessageBox.Show("You should fill the dictionary");
+            else
             {
-                MapDic = UpdateDic();
-                if (AllChar.Count == 1)
-                    break;
+                readDic(); // get dictionary
+
+                text = screen.Text; // take input from user
+
+                screen.Text = "";
+
+                string asciCd = text;
+                Allchar = new List<char>();
+                BT = new BinaryTree(MapDic, Allchar);
+
+                for (int i = 0; i < text.Length; i++)
+                {
+
+                    if (Allchar.IndexOf(text[i]) == -1)// first appearance
+                    {
+                        Allchar.Add(text[i]); // add new char
+                        BT.FirstAppear(text[i] /*send char*/, MapDic[asciCd[i]]/*send ascii of char*/, 1 /*1 mean this is compression*/);
+
+                    }
+                    else // not first appearance
+
+                        BT.NotFirstAppear(text[i], "", 1);
+
+                }
+                // print compression result
+                for (int i = 0; i < BT.compRslt.Count; i++)
+                {
+                    if (BT.compRslt.Count == 2 && i == 1)
+                        break;
+
+                    screen.Text += BT.compRslt[i] + " ";
+                }
+
             }
-            BinaryTree objBT = new BinaryTree(NumOfChar, AlterMap);
-            while ( CompMap.Count != NumOfChar  && AllChar.Count != 1 )
-                // send last two strings to add them in binary tree
-            CompMap = objBT.updateTree(AllChar[AllChar.Count - 2], AllChar[AllChar.Count - 1] , 0);
-            if (AllChar.Count == 1)
-                CompMap.Add(AllChar[0], "1");
-            screen.Text = "";
-
-
-            var items = from pair in CompMap orderby pair.Value descending select pair;
-
-            
-
-            for (int i = 0; i < text.Length; i++)
-            {
-
-                foreach (KeyValuePair<string, string> pair in items)
-
-                    if (pair.Key == text[i] + "")
-                    
-                        // print compression result 
-                        screen.Text += pair.Value + " ";
-                    
-
-            }
-
-
-
         }
+
         // Decompression
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void Button_Click_3(object sender, RoutedEventArgs e)
         {
 
-            string[] compRslt = screen.Text.Split();
-            text = screen.Text ;
-            screen.Text = "";
-            // search in dictionary
-            var items = from pair in CompMap orderby pair.Value descending select pair;
-            for (int indx = 0; indx < compRslt.Length-1; indx++)
+            if (dic.Text.Length == 0)
+                MessageBox.Show("You should fill the dictionary");
+            else
             {
 
+                readDic();
 
-               foreach (KeyValuePair<string, string> pair in items)
+                string[] compRslt = screen.Text.Split();
 
-                   if (pair.Value == compRslt[indx])
-                    
-                        screen.Text += pair.Key; 
-                    
 
+                BT = new BinaryTree(); // empty the binary tree
+                text = screen.Text;
+                screen.Text = "";
+                char c = ' ';
+                Allchar = new List<char>();
+
+                for (int i = 0; i < compRslt.Length; i++)
+                {
+                    if (Allchar.Count != MapDic.Count)
+                    {
+                        var items = from pair in MapDic select pair;
+                        foreach (KeyValuePair<char, string> pair in items) // search in dictionary 
+                        {
+                            if (compRslt[i] == pair.Value)
+                            {
+                                c = pair.Key;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (Allchar.IndexOf(c) == -1 && c != ' ') // first appearance
+                    {
+                        Allchar.Add(c);
+                        BT.FirstAppear(c, MapDic[c]/*send ascii of char*/, 2 /* 2 mean this is decompression*/);
+                        if (Allchar.Count != MapDic.Count)
+                            i++;
+                    }
+                    else // not first appearance
+                        BT.NotFirstAppear(c, compRslt[i] /*this for code in binary tree 0 or 1 or ...*/, 2);
+
+                    c = ' ';
+                }
+                screen.Text = BT.decmprsRslt;
             }
-
-            // default variable
-             MapDic.Clear();
-             CompMap.Clear(); 
-             AllChar.Clear();  
-             BinaryTree x = new BinaryTree(null) ; // head = null
-
         }
+
     }
 }
