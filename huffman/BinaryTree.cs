a5d00c25c4d77c40680c1bfba19e9c4be0b9d73b
kemo94 <kareem.hussein94@yahoo.com>
2015-03-16 11:28:23 +0200

    Huffman(Standard) Algorithm

---
+++ huffman/BinaryTree.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huffman
+{
+    class BinaryTree : MainWindow
+    {
+     
+        Dictionary<string, int> AlterDec; // Alternative dictionary
+
+        public int notFnsh = 0; // to prevent infinite loop 
+        List<Node> rest_Nodes = new List<Node>(); // all nodes that hava more than one char
+        Node restNd; // parent of rest node
+
+        public BinaryTree(int NumOfChar, Dictionary<string, int> AlterDec)
+        {
+            this.NumOfChar = NumOfChar;
+            this.AlterDec = AlterDec;
+
+        }
+        public BinaryTree(Node head) { this.head = head; }
+
+
+        public class Node  // binary tree
+        {
+            public string symbol; // char
+            public Node left, right;
+            public string code; // 0 or 1 or.... so on
+        }
+
+        public Node head = null;
+        int NumOfNode = 0; // num of nodes to compare it with num of chars
+
+        void addNode(Node Top, Node Right, Node Left, string LeftSymbol, string RightSymbol)
+        {
+
+            Top.symbol = ""; // empty symbol of parent node
+            // right
+            Right.symbol = RightSymbol;
+            Right.code = Top.code + "1";
+            Right.left = null;
+            Right.right = null;
+            // left
+            Left.symbol = LeftSymbol;
+            Left.code = Top.code + "0";
+            Left.left = null;
+            Left.right = null;
+
+            Top.right = Right;
+            Top.left = Left;
+
+
+            // add symbol to the result if just one char
+            if (Right.symbol.Length == 1)
+            {
+                CompMap.Add(Right.symbol, Right.code); // Compression result
+                NumOfNode++; // increase num of nodes
+            }
+            if (Left.symbol.Length == 1)
+            {
+
+                CompMap.Add(Left.symbol, Left.code);
+                NumOfNode++;
+            }
+
+
+            MapDic = new Dictionary<string, int>(AlterDec); // update dictionary 
+            AllChar = new List<string>(AlterDec.Keys);
+        }
+
+        public Dictionary<string, string> updateTree(string LeftSymbol, string RightSymbol, int check)
+        {
+
+            Node Top = new Node();
+            Node Right = new Node();
+            Node Left = new Node();
+
+            if (head == null)
+            {
+                Top.code = "";
+                addNode(Top, Right, Left, LeftSymbol, RightSymbol);
+                head = Top;
+            }
+            else
+            {
+                if (check == 0) // when check = 0 that mean the Top start from head 
+                {
+                    MapDic = new Dictionary<string, int>(AlterDec);
+                    AllChar = new List<string>(AlterDec.Keys);
+                    Top = head;
+                }
+                if (check == 1) // when check = 1 that mean the Top start from parent of rest node (restNd) 
+
+                    Top = restNd;
+
+                Right = Top.right;
+                Left = Top.left;
+
+
+                int chck = 0; // to check if the symbol of right node or left node has been distributed
+                // it will stop when num of all char (NumOfChar) = num of node (NumOfNode ) 
+                while (NumOfChar != NumOfNode && AllChar.Count < NumOfChar + 1)
+                {
+                    if (NumOfNode + 1 == NumOfChar)
+                        AllChar = new List<string>(AlterDec.Keys);
+
+
+                    // check  if last two char equal  the right symbol (node)
+                    if (Right.symbol != null)
+                        if (Right.symbol.Length > 1 && AllChar[AllChar.Count - 1] + AllChar[AllChar.Count - 2] == Right.symbol)
+                        {
+                            if (Top.left.symbol.Length > 1)
+                                rest_Nodes.Add(Top);
+
+                            Top = Top.right;
+                            Right = new Node();
+                            Left = new Node();
+                            addNode(Top, Right, Left, AllChar[AllChar.Count - 2], AllChar[AllChar.Count - 1]);
+
+                            chck = 1; // that mean the symbol has been distributed so continue
+
+
+                        }
+                    // check  if last two char equal  the left symbol (node)
+                    if (Left.symbol != null)
+                        if (Left.symbol.Length > 1 && AllChar[AllChar.Count - 1] + AllChar[AllChar.Count - 2] == Left.symbol)
+                        {
+
+                            if (Top.right.symbol.Length > 1)
+                                rest_Nodes.Add(Top);
+
+                            Top = Top.left;
+                            Right = new Node();
+                            Left = new Node();
+
+                            addNode(Top, Right, Left, AllChar[AllChar.Count - 2], AllChar[AllChar.Count - 1]);
+
+                            chck = 1; // that mean the symbol has been distributed so continue
+
+                        }
+
+               
+                    if (Right.symbol != null)
+                        if (Right.symbol.Length == 1)
+                            if (Top.left.symbol.Length > 1 && rest_Nodes.IndexOf(Top) == -1 ) 
+                                rest_Nodes.Add(Top);
+
+                    if (Left.symbol != null)
+                        if (Left.symbol.Length == 1)
+                            if (Top.right.symbol.Length > 1 && rest_Nodes.IndexOf(Top) == -1 )
+                                rest_Nodes.Add(Top);
+
+
+
+                    notFnsh++;
+                    if (notFnsh > 3)
+                    {
+                        AllChar = new List<string>(AlterDec.Keys);
+                        notFnsh = 0;
+                    }
+                    // to updete dictionary (MapDec)
+                    else if (chck == 1 || (Left.symbol.Length >= AllChar[AllChar.Count - 2].Length + AllChar[AllChar.Count - 1].Length ||
+                        Right.symbol.Length >= AllChar[AllChar.Count - 2].Length + AllChar[AllChar.Count - 1].Length))
+                    {
+                        chck  = 0 ;
+                        // to updete dictionary (MapDec)
+                        MapDic = UpdateDic();
+                        AllChar = new List<string>(MapDic.Keys);
+                    }
+                    else  
+                         break;
+
+              
+                }
+            }
+
+
+                 while ( rest_Nodes.Count != 0 ) // get rest nodes
+                {
+                    restNd = rest_Nodes[0];
+                    rest_Nodes.Remove(rest_Nodes[0]); // delete the taken rest node
+
+                    if (restNd.left.symbol.Length > 1 || restNd.right.symbol.Length > 1 )
+                    updateTree(AllChar[AllChar.Count - 1], AllChar[AllChar.Count - 2], 1);
+
+                }
+            return CompMap;
+        }
+
+    }
+}
+
---
+++ huffman/MainWindow.xaml.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections;
+namespace huffman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Dictionary<string, int> MapDic = new Dictionary<string, int>(); // dictionary for Compression
+        public Dictionary<string, string> CompMap = new Dictionary<string, string>(); // Compression result
+
+        public List<string> AllChar = new List<string>(); // All Charcters without duplicate 
+
+        string text = ""; // take data from user
+        public int back = 0;
+        public int NumOfChar = 0;
+
+        void getDuplicated() // get sum of duplicated char 
+        {
+
+            Dictionary<string, int> Map = new Dictionary<string, int>();
+            Map.Add((screen.Text[0] + ""), 1);
+            var items = from pair in Map orderby pair.Value descending select pair;
+            int check = 0;
+            for (int i = 1; i < screen.Text.Length; i++)
+            {
+
+
+                items = from pair in Map orderby pair.Value descending select pair;
+
+                foreach (KeyValuePair<string, int> pair in items) // count repeated char
+                {
+                    if (pair.Key != screen.Text[i] + "") // if not found add it in map
+
+                        check = 1;
+
+                    else // if found increase the counter
+                    {
+                        Map[pair.Key]++;
+
+                        check = 0;
+                        break;
+                    }
+                }
+
+
+
+                if (check == 1)
+                {
+
+                    Map.Add(screen.Text[i] + "", 1);
+                    check = 0;
+                }
+            }
+
+
+            //sort map
+            items = from pair in Map orderby pair.Value descending select pair;
+            foreach (KeyValuePair<string, int> pair in items)
+            {
+                MapDic.Add(pair.Key, pair.Value);
+                AllChar.Add(pair.Key);
+                 
+            }
+            NumOfChar = Map.Count;
+        }
+
+        public Dictionary<string, int> UpdateDic()
+        {
+
+
+            List<string> listChar = new List<string>(MapDic.Keys); 
+            Dictionary<string, int> BxMap = new Dictionary<string, int>(); // update map
+
+            int  sum = 0;
+            string chars = "";
+            for (int indx = 0; indx < listChar.Count; indx ++)
+            {
+                if (indx == MapDic.Count - 2 || indx == MapDic.Count - 1)
+                {
+                    chars = listChar[indx] + chars;
+                    sum += MapDic[listChar[indx]];
+                }
+
+                else
+
+                    BxMap.Add(listChar[indx], MapDic[listChar[indx]]);
+
+
+
+                if (indx == MapDic.Count - 1) // add last two string together 
+
+                    BxMap.Add(chars, sum);
+
+            }
+            // sort map
+            var items = from pair in BxMap orderby pair.Value descending select pair;
+            MapDic.Clear();
+            AllChar.Clear();
+
+            foreach (KeyValuePair<string, int> pair in items)
+            {
+                MapDic.Add(pair.Key, pair.Value);
+                AllChar.Add(pair.Key);
+
+            }
+            return MapDic;
+        }
+
+        // Compression
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            text = screen.Text;
+            getDuplicated(); // count sum of duplicated char
+
+
+            Dictionary<string, int> AlterMap = new Dictionary<string, int>(MapDic);
+            List<string>  AlterAllChar = new List<string>(AllChar);
+
+            while (MapDic.Count != 2 ) // decrease the map to reach 0 , 1 (left & right) (binary tree) 
+            {
+                MapDic = UpdateDic();
+                if (AllChar.Count == 1)
+                    break;
+            }
+            BinaryTree objBT = new BinaryTree(NumOfChar, AlterMap);
+            while ( CompMap.Count != NumOfChar  && AllChar.Count != 1 )
+                // send last two strings to add them in binary tree
+            CompMap = objBT.updateTree(AllChar[AllChar.Count - 2], AllChar[AllChar.Count - 1] , 0);
+            if (AllChar.Count == 1)
+                CompMap.Add(AllChar[0], "1");
+            screen.Text = "";
+
+
+            var items = from pair in CompMap orderby pair.Value descending select pair;
+
+            
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                foreach (KeyValuePair<string, string> pair in items)
+
+                    if (pair.Key == text[i] + "")
+                    
+                        // print compression result 
+                        screen.Text += pair.Value + " ";
+                    
+
+            }
+
+
+
+        }
+        // Decompression
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            string[] compRslt = screen.Text.Split();
+            text = screen.Text ;
+            screen.Text = "";
+            // search in dictionary
+            var items = from pair in CompMap orderby pair.Value descending select pair;
+            for (int indx = 0; indx < compRslt.Length-1; indx++)
+            {
+
+
+               foreach (KeyValuePair<string, string> pair in items)
+
+                   if (pair.Value == compRslt[indx])
+                    
+                        screen.Text += pair.Key; 
+                    
+
+            }
+
+            // default variable
+             MapDic.Clear();
+             CompMap.Clear(); 
+             AllChar.Clear();  
+             BinaryTree x = new BinaryTree(null) ; // head = null
+
+        }
+    }
+}
